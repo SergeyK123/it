d54d8dac9e6ddd6e941b8fcdf92cfdfab5084648
SergeyK123 <75069371+SergeyK123@users.noreply.github.com>
2020-11-30 15:42:58 +0600

    Create project_2.cpp

---
+++ project_2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include<string>
+int main()
+{
+    std::string information, action;
+    std::cin >>information;
+    std::cout << "say number" <<std:: endl;
+    std::cin >> action;
+    std::cout << "take key" << std::endl;
+    return 0;
+
+
+}
